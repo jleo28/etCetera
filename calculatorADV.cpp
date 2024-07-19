b8c7197ba8dcdf13d78101ebc8d8d411e02d9e20
Joseph Leo <158729837+jleo28@users.noreply.github.com>
2024-07-19 12:38:29 +0700

    Update and rename calculatorADV to calculatorADV.cpp

--- calculatorADV.cpp
+++ calculatorADV.cpp
@@ -1,7 +1,11 @@
 //Hi! As part of a self-paced course in tinkering with my C++ skills, I'll be attempting to write code for a Complex Calculator in C++.
+//I'll be writing the base code here on Github, but I'll be running parallel tests on VSCode.
+//The reason I'm doing this is just to get a better feel for continuous workflow on Github ^-^.
 
 #include <iostream>
 #include <cmath>
 #include <map>
 #incldue <functional>
 #include <stdexcept>
+
+class Calc 
